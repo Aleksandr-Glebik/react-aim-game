d8330d930142887abd402ae4366202f62ffc3b0e
Aleksandr-Glebik <glebikaa@mail.ru>
2023-05-05 14:38:21 +0300

    feat: add markup and style for app

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,44 @@ import './scss/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      Start project
+    <div className="app">
+      <div className="app__header">
+        <h1 id="time-header">
+          Время игры:
+          <span id="time"> 5.0</span>
+        </h1>
+        <h1 id="result-header">
+          Результат игры:
+          <span id="result"> 0</span>
+        </h1>
+      </div>
+      <div className="app__content">
+        <button className="btn" id="start-game">
+          Начать
+        </button>
+        <div className="game__field" id="game-field">
+        </div>
+      </div>
+      <div className="app__footer">
+        <div className="input__container">
+          <label
+            htmlFor="game-time"
+            className="input__container_label"
+          >
+            Время игры, (сек)
+          </label>
+          <input
+            className="input__container_input"
+            type="number"
+            id="game-time"
+            min="5"
+            value={5}
+            step="1"
+          />
+        </div>
+      </div>
     </div>
+
   );
 }
 
